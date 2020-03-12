7647a57b78988e63b127ff6ae7d7aa7726237c5f
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-03-12 20:56:55 +0900

    [finish]3-7,8,9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -20,11 +20,24 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+  def destroy
+    list = List.find(params[:id]) #データ（レコード）を１件取得
+    list.destroy #データ（レコード）を削除
+    redirect_to todolists_path #List一覧画面へリダイレクト
+  end
 
   private
 
   def list_params
-  	 params.require(:list).permit(:title, :body)
+  	 params.require(:list).permit(:title, :body, :image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '552590cb7934104cb819027330a18498610cf1ff9ee596c6a71096ef62a724c406081c524a8d1d06e4dd273733f2de0ba181052c5376b19f3b5bbbbd355e02ad'
